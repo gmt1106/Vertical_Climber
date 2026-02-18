ebe6ea8d46028823d10f4f9a4b24d00e6672b8c6
gmt1106 <gmt1106@gmail.com>
2026-02-18 10:12:15 -0800

    updated the android compileSDK version from 34 to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "com.yourpackage.mountaingoat"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.yourpackage.mountaingoat"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
